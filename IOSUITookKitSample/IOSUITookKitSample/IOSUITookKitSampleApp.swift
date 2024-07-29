0b266e6a74073203baa65590ab3f802657c0e6e5
murali-elite <murali@elitecorpusa.com>
2024-07-29 16:49:13 +0530

    Initial Commit

---
+++ IOSUITookKitSample/IOSUITookKitSample/IOSUITookKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSUITookKitSampleApp.swift
+//  IOSUITookKitSample
+//
+//  Created by Murali moorthy on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct IOSUITookKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
